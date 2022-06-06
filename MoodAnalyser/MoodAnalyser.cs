2bd967f8565635bb675e00a3cf99632f9137a3a9
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-06-06 12:39:27 +0530

    [Nantha] - Mood Analyser Created

---
+++ MoodAnalyser/MoodAnalyser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mood_Analyser
+{
+    public class MoodAnalyser
+    {
+        public string AnalyseMood(string mood)
+        {
+            if (mood.Contains("sad"))
+            {
+                Console.WriteLine("Input Contains Sad");
+                return "Sad";
+            }
+            else
+            {
+                Console.WriteLine("Input Contains Happy");
+                return "Happy";
+            }
+        }
+        
+    }
+}
---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,2 @@
+﻿Mood_Analyser.MoodAnalyser checkMood = new Mood_Analyser.MoodAnalyser();
+checkMood.AnalyseMood("I am in sad mood");
