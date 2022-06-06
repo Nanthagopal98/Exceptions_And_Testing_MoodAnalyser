9cebf2c362d4c48687f4036dc418ee3aa9e9803f
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-06-06 18:11:30 +0530

    [Nantha] - Test Case Created For Null Value

--- MoodAnalyserTest/TestMood.cs
+++ MoodAnalyserTest/TestMood.cs
@@ -16,7 +16,18 @@ namespace MoodAnalyserTest
             string actualResult = checkMood.AnalyseMood();
             Assert.AreNotEqual("Sad", actualResult);
         }
-        
-
+        [Test]
+        public void GivenMoodNull_WhenCheckMood_ThenThrowException()
+        {
+            try
+            {
+                Mood_Analyser.MoodAnalyser checkMood = new Mood_Analyser.MoodAnalyser(null);
+                string actualResult = checkMood.AnalyseMood();
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual("Mood Should not be null", e.Message);
+            }
+        }
     }
 }
