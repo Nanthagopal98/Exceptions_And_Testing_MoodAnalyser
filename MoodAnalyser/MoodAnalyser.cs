f092196cef49f3049a8e5f1ddc2f98882e87428f
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-06-06 15:35:43 +0530

    [Nantha] - Try Catch Block Added

--- MoodAnalyser/MoodAnalyser.cs
+++ MoodAnalyser/MoodAnalyser.cs
@@ -13,16 +13,24 @@ namespace Mood_Analyser
         {
             this.mood = mood;
         }
-            public string AnalyseMood()
+        public string AnalyseMood()
         {
-            if (this.mood.Contains("sad"))
+            try
             {
-                Console.WriteLine("Input Contains Sad");
-                return "Sad";
+                if (this.mood.Contains("sad"))
+                {
+                    Console.WriteLine("Input Contains Sad");
+                    return "Sad";
+                }
+                else
+                {
+                    Console.WriteLine("Input Contains Happy");
+                    return "Happy";
+                }
             }
-            else
+            catch
             {
-                Console.WriteLine("Input Contains Happy");
+                Console.WriteLine("Null Value Present");
                 return "Happy";
             }
         }
