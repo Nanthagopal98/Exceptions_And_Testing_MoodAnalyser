11a21d7339727d3a8ba8a348e40a773bbb5a9a52
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-06-06 15:40:10 +0530

    [Nantha] - Null Value Entered and Carch Block Executed

--- MoodAnalyserTest/TestMood.cs
+++ MoodAnalyserTest/TestMood.cs
@@ -16,6 +16,13 @@ namespace MoodAnalyserTest
             string actualResult = checkMood.AnalyseMood();
             Assert.AreNotEqual("Sad", actualResult);
         }
+        [Test]
+        public void GivenMoodNull_CheckMood_ReturnResultAsHappy()
+        {
+            Mood_Analyser.MoodAnalyser checkMood = new Mood_Analyser.MoodAnalyser(null);
+            string actualResult = checkMood.AnalyseMood();
+            Assert.AreNotEqual("Sad", actualResult);
+        }
 
     }
 }
