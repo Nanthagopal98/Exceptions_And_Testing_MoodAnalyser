a68f45d8763c910dc39f50e091af30f237529b12
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-06-06 12:51:15 +0530

    [Nantha] - Happy mood Test Done

--- MoodAnalyserTest/TestMood.cs
+++ MoodAnalyserTest/TestMood.cs
@@ -9,6 +9,13 @@ namespace MoodAnalyserTest
             string actualResult = checkMood.AnalyseMood("I am in sad mood");
             Assert.AreEqual("Sad", actualResult);
         }
-        
+        [Test]
+        public void GivenMood_CheckMood_ReturnResultAsHappy()
+        {
+            Mood_Analyser.MoodAnalyser checkMood = new Mood_Analyser.MoodAnalyser();
+            string actualResult = checkMood.AnalyseMood("Hai, Today i am in any mood");
+            Assert.AreNotEqual("Sad", actualResult);
+        }
+
     }
 }
